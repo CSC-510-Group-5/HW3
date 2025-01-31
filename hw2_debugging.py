0dc985d6eb6f43859f0f204d87a48d6c8e9a963d
cablumsa <cablumsa@ncsu.edu>
2025-01-31 13:57:23 -0500

    Making fixes from pyflakes

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -2,7 +2,7 @@ import rand
 
 def selectionSort(arr):
     for i in range(len(arr)):
-        min_idx = 
+        min_idx = i
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_idx]:
                 min_idx = j
