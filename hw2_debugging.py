90a5e032cb4d969952006fcb1fa89393491e2647
cablumsa <cablumsa@ncsu.edu>
2025-01-31 13:43:01 -0500

    Creating selection sort

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,5 +1,14 @@
 import rand
 
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
 def mergeSort(arr):
     if (len(arr) == 1):
         return arr
